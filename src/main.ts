131b5ffc05010906a944bfefae56f19dd2cc59e5
Kevin Antonio Simbana Morocho <ka.simbana@alumnos.upm.es>
2020-06-16 19:46:08 +0200

    add ValidationPipe #1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
 bootstrap();
