45249c71dc02371b479f50072382a0b80210a2ac
Kevin Antonio Simbana Morocho <ka.simbana@alumnos.upm.es>
2020-06-16 18:45:52 +0200

    add mongoose config.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
-import { MongooseModule } from "@nestjs/mongoose";
+import { MongooseModule, MongooseModuleOptions } from "@nestjs/mongoose";
+
+const MONGOOSE_CONFIG: MongooseModuleOptions = {
+    useCreateIndex: true
+}
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
-        MongooseModule.forRoot(process.env.MONGODB_URI)
+        MongooseModule.forRoot(process.env.MONGODB_URI, MONGOOSE_CONFIG)
     ],
     controllers: [AppController],
     providers: [AppService]
