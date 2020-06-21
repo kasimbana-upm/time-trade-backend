9fd7450b591857b16dd30761aec454a590590efa
Kevin Simbana Morocho <ka.simbana@alumnos.upm.es>
2020-06-21 03:43:34 +0200

    add create user #1

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from 'src/schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +10,10 @@ export class UsersController {
     @Get()
     findAll(): Promise<User[]> {
         return this.usersService.findAll();
+    }
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return this.usersService.create(createUserDto);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/schemas/user.schema';
+import { User } from '../schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -9,5 +10,10 @@ export class UsersService {
 
     findAll(): Promise<User[]> {
         return this.userModel.find().exec();
+    }
+
+    create(createUserDto: CreateUserDto): Promise<User> {
+        const createdUser = new this.userModel(createUserDto);
+        return createdUser.save();
     }
 }
